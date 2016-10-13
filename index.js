819c4b8a3a3a6d949a44453f83eebb91405ff1b0
drm2 <davidrmyersii@gmail.com>
2016-10-13 15:02:36 -0400

    Removing old code from index.js.

--- index.js
+++ index.js
@@ -11,33 +11,3 @@ var bot = new TwitterBot({
     access_token: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
-
-
-// test for [TwitterBot].schedule
-// returning the resolver to chain onto the scheduler (useful for action chaining)
-// var handler = bot.schedule(function () {
-//     return bot.tweet('[bot test] this tweet is delayed.');
-// }, new Date((new Date()).getTime() + (5 * 1000)))
-//     .then(function (result) {
-//         console.log(result.data);
-//     });
-//
-// handler.cancel();
-
-// chaining onto the resolver inside the scheduler (useful for triggering multiple actions at once)
-// var handler = bot.schedule(function () {
-//     bot.tweet('[bot test] this tweet is delayed.')
-//         .then(function (result) {
-//             console.log('Data:', result.data);
-//         })
-//         .catch(function (error) {
-//             console.log('Error:', error);
-//         });
-//
-//     // possibly perform multiple actions in one schedule instance
-// }, new Date((new Date()).getTime() + (5 * 1000)))
-//     .then(function () {
-//         // nothing passed but the Promise still resolves when the action finishes
-//     });
-//
-// handler.cancel();
